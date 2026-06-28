8a4c8a52faa524656ea97eea48fcc6deb4394a2c
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-27 23:55:04 -0500

    Create mining.js

---
+++ data/mining.js
@@ -0,0 +1,358 @@
+export const miningProducts = [
+
+{
+id: "SHD-GC",
+
+```
+family: "Minería",
+
+category: "Trailing Cable",
+
+name: "SHD-GC",
+
+conductor: "Cobre flexible",
+
+insulation: "EPR",
+
+jacket: "CPE",
+
+voltage: "5 kV - 25 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Equipos móviles",
+    "Minería a cielo abierto",
+    "Minería subterránea"
+],
+
+applications: [
+    "Palas eléctricas",
+    "Draglines",
+    "Excavadoras",
+    "Trituradoras móviles"
+],
+
+features: [
+    "Alta flexibilidad",
+    "Alta resistencia mecánica",
+    "Resistente a abrasión",
+    "Resistente a aceites"
+],
+
+tags: [
+    "mineria",
+    "shd-gc",
+    "trailing",
+    "cable movil",
+    "trituradora",
+    "pala electrica",
+    "dragline"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/shdgc.jpg"
+```
+
+},
+
+{
+id: "G-GC",
+
+```
+family: "Minería",
+
+category: "Power Cable",
+
+name: "G-GC",
+
+conductor: "Cobre flexible",
+
+insulation: "EPR",
+
+jacket: "CPE",
+
+voltage: "2000 V",
+
+temperature: "90°C",
+
+installation: [
+    "Bandejas",
+    "Canalizaciones",
+    "Equipos industriales"
+],
+
+applications: [
+    "Alimentación de equipos mineros",
+    "Subestaciones móviles",
+    "Centros de control"
+],
+
+features: [
+    "Flexible",
+    "Resistente a impacto",
+    "Resistente a humedad"
+],
+
+tags: [
+    "g-gc",
+    "mineria",
+    "alimentador",
+    "industrial"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/ggc.jpg"
+```
+
+},
+
+{
+id: "MP-GC",
+
+```
+family: "Minería",
+
+category: "Portable Power",
+
+name: "MP-GC",
+
+conductor: "Cobre flexible",
+
+insulation: "EPR",
+
+jacket: "CPE",
+
+voltage: "2 kV - 15 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Equipos portátiles",
+    "Minería"
+],
+
+applications: [
+    "Bombas",
+    "Transportadores",
+    "Trituración"
+],
+
+features: [
+    "Alta flexibilidad",
+    "Servicio pesado"
+],
+
+tags: [
+    "mp-gc",
+    "portable",
+    "mineria",
+    "bomba",
+    "transportador"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/mpgc.jpg"
+```
+
+},
+
+{
+id: "REELING-CABLE",
+
+```
+family: "Minería",
+
+category: "Reeling",
+
+name: "Cable Reeling",
+
+conductor: "Cobre flexible",
+
+insulation: "EPR",
+
+jacket: "Poliuretano",
+
+voltage: "0.6/1 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Carretes automáticos",
+    "Enrolladores"
+],
+
+applications: [
+    "Grúas",
+    "Apiladores",
+    "Recuperadores"
+],
+
+features: [
+    "Flexión continua",
+    "Alta vida útil"
+],
+
+tags: [
+    "reeling",
+    "carrete",
+    "enrollable",
+    "grua"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/reeling.jpg"
+```
+
+},
+
+{
+id: "FESTOON-CABLE",
+
+```
+family: "Minería",
+
+category: "Festoon",
+
+name: "Cable Festoon",
+
+conductor: "Cobre flexible",
+
+insulation: "PVC/EPR",
+
+voltage: "0.6/1 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Sistemas festoon",
+    "Puentes grúa"
+],
+
+applications: [
+    "Movimiento horizontal",
+    "Equipos móviles"
+],
+
+features: [
+    "Alta flexibilidad",
+    "Resistencia mecánica"
+],
+
+tags: [
+    "festoon",
+    "puente grua",
+    "movil"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/festoon.jpg"
+```
+
+},
+
+{
+id: "DRAG-CHAIN",
+
+```
+family: "Minería",
+
+category: "Cadena Portacables",
+
+name: "Drag Chain Cable",
+
+conductor: "Cobre ultraflexible",
+
+insulation: "TPE",
+
+voltage: "300/500 V",
+
+temperature: "80°C",
+
+installation: [
+    "Cadenas portacables",
+    "Movimiento continuo"
+],
+
+applications: [
+    "Automatización",
+    "Equipos móviles",
+    "Máquinas mineras"
+],
+
+features: [
+    "Millones de ciclos",
+    "Radio de curvatura reducido"
+],
+
+tags: [
+    "drag chain",
+    "cadena portacables",
+    "automatizacion",
+    "movimiento continuo"
+],
+
+brochure: "MINING",
+
+image: "assets/cables/dragchain.jpg"
+```
+
+},
+
+{
+id: "SUBMERSIBLE-MINE",
+
+```
+family: "Minería",
+
+category: "Bombas",
+
+name: "Cable para Bomba Sumergible",
+
+conductor: "Cobre",
+
+insulation: "EPR",
+
+jacket: "CPE",
+
+voltage: "600 V",
+
+temperature: "90°C",
+
+installation: [
+    "Pozos",
+    "Piscinas de relaves",
+    "Bombas sumergibles"
+],
+
+applications: [
+    "Drenaje minero",
+    "Bombeo industrial"
+],
+
+features: [
+    "Resistente al agua",
+    "Resistente a químicos"
+],
+
+tags: [
+    "sumergible",
+    "bomba",
+    "agua",
+    "pozo",
+    "mineria"
+],
+
+brochure: "SUBMERSIBLE",
+
+image: "assets/cables/submersible.jpg"
+```
+
+}
+
+];
