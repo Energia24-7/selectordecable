6222fd9b15fd86a532b1ecf8d76a0f32665b25a6
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-28 00:46:15 -0500

    Update renewable.js

--- data/renewable.js
+++ data/renewable.js
@@ -1,231 +1,78 @@
 export const renewableProducts = [
 
 {
-id: "PRYSOLAR-H1Z2Z2K",
+    id: "PRYSOLAR-H1Z2Z2K",
 
-```
-family: "Energía Renovable",
+    family: "Energía Solar",
 
-category: "Fotovoltaico",
+    category: "Fotovoltaico",
 
-name: "Prysolar H1Z2Z2-K",
+    name: "Prysolar H1Z2Z2-K",
 
-conductor: "Cobre estañado flexible Clase 5",
+    conductor: "Cobre estañado",
 
-insulation: "XLPO",
+    insulation: "XLPO",
 
-voltage: "1500 VDC",
+    voltage: "1500 VDC",
 
-temperature: "-40°C a 90°C",
+    temperature: "-40°C a 90°C",
 
-installation: [
-    "Exterior",
-    "Canalizaciones",
-    "Bandejas",
-    "Instalación solar"
-],
+    installation: [
+        "Exterior",
+        "Bandejas",
+        "Canalizaciones"
+    ],
 
-applications: [
-    "Strings DC",
-    "Paneles fotovoltaicos",
-    "Conexión entre módulos"
-],
+    applications: [
+        "Sistemas fotovoltaicos",
+        "String DC",
+        "Parques solares"
+    ],
 
-standards: [
-    "EN 50618",
-    "IEC 62930"
-],
+    tags: [
+        "solar",
+        "fotovoltaico",
+        "panel",
+        "dc",
+        "1500v"
+    ],
 
-tags: [
-    "solar",
-    "fotovoltaico",
-    "1500vdc",
-    "panel",
-    "string",
-    "dc",
-    "prysolar",
-    "h1z2z2-k"
-],
-
-brochure: "SOLAR",
-
-image: "assets/cables/prysolar.jpg"
-```
-
+    brochure: "SOLAR"
 },
 
 {
-id: "PV-INVERTER-AC",
+    id: "PV-AL-1500",
 
-```
-family: "Energía Renovable",
+    family: "Energía Solar",
 
-category: "Interconexión AC",
+    category: "Fotovoltaico",
 
-name: "Cable AC para Inversores",
+    name: "Cable Solar Aluminio 1500V",
 
-conductor: "Cobre",
+    conductor: "Aluminio",
 
-insulation: "XLPE",
+    insulation: "XLPO",
 
-voltage: "0.6/1 kV",
+    voltage: "1500 VDC",
 
-temperature: "90°C",
+    temperature: "90°C",
 
-installation: [
-    "Bandejas",
-    "Canalizaciones",
-    "Exterior"
-],
+    installation: [
+        "Parques solares"
+    ],
 
-applications: [
-    "Salida AC de inversores",
-    "Conexión a tableros",
-    "Plantas solares"
-],
+    applications: [
+        "Alimentadores DC"
+    ],
 
-tags: [
-    "inversor",
-    "ac",
-    "solar",
-    "xlpe",
-    "0.6/1kv"
-],
+    tags: [
+        "solar",
+        "aluminio",
+        "1500v",
+        "fotovoltaico"
+    ],
 
-brochure: "SOLAR",
-
-image: "assets/cables/inverter-ac.jpg"
-```
-
-},
-
-{
-id: "PV-TRACKER",
-
-```
-family: "Energía Renovable",
-
-category: "Tracking",
-
-name: "Cable para Sistemas Tracker",
-
-conductor: "Cobre flexible",
-
-insulation: "XLPO",
-
-voltage: "1000 V",
-
-temperature: "90°C",
-
-installation: [
-    "Movimiento continuo",
-    "Exterior"
-],
-
-applications: [
-    "Trackers solares",
-    "Equipos móviles"
-],
-
-tags: [
-    "tracker",
-    "solar",
-    "movimiento",
-    "seguidor solar"
-],
-
-brochure: "SOLAR",
-
-image: "assets/cables/tracker.jpg"
-```
-
-},
-
-{
-id: "PV-COMBINER",
-
-```
-family: "Energía Renovable",
-
-category: "Balance of System",
-
-name: "Cable para Combiner Box",
-
-conductor: "Cobre",
-
-insulation: "XLPE",
-
-voltage: "1500 VDC",
-
-temperature: "90°C",
-
-installation: [
-    "Canalización",
-    "Gabinetes"
-],
-
-applications: [
-    "Combiner Box",
-    "String Box"
-],
-
-tags: [
-    "combiner",
-    "string box",
-    "1500vdc",
-    "solar"
-],
-
-brochure: "SOLAR",
-
-image: "assets/cables/combiner.jpg"
-```
-
-},
-
-{
-id: "PV-MV-COLLECTION",
-
-```
-family: "Energía Renovable",
-
-category: "Colección MT",
-
-name: "Cable XLPE Media Tensión Solar",
-
-conductor: "Cobre o Aluminio",
-
-insulation: "XLPE",
-
-voltage: "15 kV - 35 kV",
-
-temperature: "90°C",
-
-installation: [
-    "Enterrado",
-    "Ductos",
-    "Bandejas"
-],
-
-applications: [
-    "Colectores MT",
-    "Subestaciones solares",
-    "Parques fotovoltaicos"
-],
-
-tags: [
-    "media tension",
-    "solar farm",
-    "15kv",
-    "25kv",
-    "35kv"
-],
-
-brochure: "SOLAR",
-
-image: "assets/cables/mt-solar.jpg"
-```
-
+    brochure: "SOLAR"
 }
 
 ];
