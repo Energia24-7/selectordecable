84835ba62e4f5f299ab56e3646a8ebaa4de5ede5
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-27 23:53:58 -0500

    Create renewableProducts module with specifications
    Added a new module for renewable products with detailed specifications including categories, applications, and tags.

---
+++ data/renewable.js
@@ -0,0 +1,231 @@
+export const renewableProducts = [
+
+{
+id: "PRYSOLAR-H1Z2Z2K",
+
+```
+family: "Energía Renovable",
+
+category: "Fotovoltaico",
+
+name: "Prysolar H1Z2Z2-K",
+
+conductor: "Cobre estañado flexible Clase 5",
+
+insulation: "XLPO",
+
+voltage: "1500 VDC",
+
+temperature: "-40°C a 90°C",
+
+installation: [
+    "Exterior",
+    "Canalizaciones",
+    "Bandejas",
+    "Instalación solar"
+],
+
+applications: [
+    "Strings DC",
+    "Paneles fotovoltaicos",
+    "Conexión entre módulos"
+],
+
+standards: [
+    "EN 50618",
+    "IEC 62930"
+],
+
+tags: [
+    "solar",
+    "fotovoltaico",
+    "1500vdc",
+    "panel",
+    "string",
+    "dc",
+    "prysolar",
+    "h1z2z2-k"
+],
+
+brochure: "SOLAR",
+
+image: "assets/cables/prysolar.jpg"
+```
+
+},
+
+{
+id: "PV-INVERTER-AC",
+
+```
+family: "Energía Renovable",
+
+category: "Interconexión AC",
+
+name: "Cable AC para Inversores",
+
+conductor: "Cobre",
+
+insulation: "XLPE",
+
+voltage: "0.6/1 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Bandejas",
+    "Canalizaciones",
+    "Exterior"
+],
+
+applications: [
+    "Salida AC de inversores",
+    "Conexión a tableros",
+    "Plantas solares"
+],
+
+tags: [
+    "inversor",
+    "ac",
+    "solar",
+    "xlpe",
+    "0.6/1kv"
+],
+
+brochure: "SOLAR",
+
+image: "assets/cables/inverter-ac.jpg"
+```
+
+},
+
+{
+id: "PV-TRACKER",
+
+```
+family: "Energía Renovable",
+
+category: "Tracking",
+
+name: "Cable para Sistemas Tracker",
+
+conductor: "Cobre flexible",
+
+insulation: "XLPO",
+
+voltage: "1000 V",
+
+temperature: "90°C",
+
+installation: [
+    "Movimiento continuo",
+    "Exterior"
+],
+
+applications: [
+    "Trackers solares",
+    "Equipos móviles"
+],
+
+tags: [
+    "tracker",
+    "solar",
+    "movimiento",
+    "seguidor solar"
+],
+
+brochure: "SOLAR",
+
+image: "assets/cables/tracker.jpg"
+```
+
+},
+
+{
+id: "PV-COMBINER",
+
+```
+family: "Energía Renovable",
+
+category: "Balance of System",
+
+name: "Cable para Combiner Box",
+
+conductor: "Cobre",
+
+insulation: "XLPE",
+
+voltage: "1500 VDC",
+
+temperature: "90°C",
+
+installation: [
+    "Canalización",
+    "Gabinetes"
+],
+
+applications: [
+    "Combiner Box",
+    "String Box"
+],
+
+tags: [
+    "combiner",
+    "string box",
+    "1500vdc",
+    "solar"
+],
+
+brochure: "SOLAR",
+
+image: "assets/cables/combiner.jpg"
+```
+
+},
+
+{
+id: "PV-MV-COLLECTION",
+
+```
+family: "Energía Renovable",
+
+category: "Colección MT",
+
+name: "Cable XLPE Media Tensión Solar",
+
+conductor: "Cobre o Aluminio",
+
+insulation: "XLPE",
+
+voltage: "15 kV - 35 kV",
+
+temperature: "90°C",
+
+installation: [
+    "Enterrado",
+    "Ductos",
+    "Bandejas"
+],
+
+applications: [
+    "Colectores MT",
+    "Subestaciones solares",
+    "Parques fotovoltaicos"
+],
+
+tags: [
+    "media tension",
+    "solar farm",
+    "15kv",
+    "25kv",
+    "35kv"
+],
+
+brochure: "SOLAR",
+
+image: "assets/cables/mt-solar.jpg"
+```
+
+}
+
+];
